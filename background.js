f10030f0cd79d9dacec429a6dbdaedbe47cead7c
Matt Huber <matt@edecdf.com>
2018-11-16 15:19:20 -0800

    Added dynamic load of graph data, fixed mouseover display

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-console.log("hello");
 
+//need to monitor when history changed since twitch doesn't trigger DOM loaded event when changing pages
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {historyChanged: "true"}, function(response) {});
       });
-    console.log('Page uses History API and we heard a pushSate/replaceState.');
+    // console.log('Page uses History API and we heard a pushSate/replaceState.');
     });
--- display-stats.js
+++ display-stats.js
@@ -1,8 +1,8 @@
-//TODO gradually display graph as data is pulled from twitch
 //TODO annotate popular points for quick reference
 //TODO do in depth analysis for emotes - in the background
 //TODO V2 - sync chat log with remote server to improve initial pull speed
-//TODO: Add loading screen for loading graph/pulling from server (merge w/ first TODO)
+//TODO Load embedded player for faster seek, give option to switch between native and embedded?
+//TODO add help button/mouseover to show controls and other info
 
 var videoId;
 var dyGraph = null;
@@ -20,6 +20,7 @@ class ChatMessage {
 
 window.addEventListener ("load", insertLoadGraphButton, false);
 
+//if browser history changed and url matches twitch VOD, reset graph display
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.historyChanged == "true"){
@@ -36,8 +37,8 @@ function resetGraph(){
     chatlogs = {};
     if (dyGraph != null){
         dyGraph.destroy();
-        document.getElementById("frequencyChart").style.visibility = "collapsed";
-        document.getElementById("reset-chat-button").style.visibility = "collapsed";
+        document.getElementById("frequencyChart").style.visibility = "collapse";
+        document.getElementById("reset-chat-button").style.visibility = "collapse";
     }
 
     insertLoadGraphButton();
@@ -68,11 +69,13 @@ function insertLoadGraphButton(){
 
 
 function loadGraph(evt){
-    document.getElementById("load-chat-button").style.visibility = "collapsed";
+    document.getElementById("load-chat-button").style.visibility = "collapse";
     document.getElementById("frequencyChart").style.visibility = "visible";
     video = (document.URL).split("/")[4];
     videoId = video.split("?")[0];
     console.log("Video Id: " + videoId);
+
+    displayDyGraph([[1,0]]);
 
     chrome.storage.local.get(['chatlogs'], function(result) {
         console.log('Value currently is ' + result.chatlogs[videoId]);
@@ -81,21 +84,18 @@ function loadGraph(evt){
             parseSavedChatLog(result.chatlogs[videoId]);
         }
         else{
-            pullChatLog([], null);
+            pullChatLog([], null, null, null, null);
         }
     });
 }
 
-// input time in seconds, returns string with hh:mm:ss
+// input time in seconds, returns string with format hh:mm:ss
 function secondsToHms(d) {
     d = Number(d);
     var h = Math.floor(d / 3600);
     var m = Math.floor(d % 3600 / 60);
     var s = Math.floor(d % 3600 % 60);
 
-    // var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    // var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    // var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
     var hDisplay = h > 0 ? h + (h == 1 ? ":" : ":") : "";
     var mDisplay = m > 0 ? m + (m == 1 ? ":" : ":") : "";
     var sDisplay = s > 0 ? s + (s == 1 ? "" : "") : "";
@@ -106,28 +106,24 @@ function secondsToHms(d) {
 function parseSavedChatLog(chatlog){
     var time = 5;
     var frequencyValue = 0;
-    //var frequencyData = []; //list of comment count every time interval
-    //var timeData = []   //timestamps for frequencyData array
 
     var graphData = [];
 
     for (i = 0; i < chatlog.length; i++){
         if (chatlog[i].time > time){
-            //frequencyData.push(frequencyValue);
-            //timeData.push(secondsToHms(time));
             graphData.push([time,frequencyValue]);
-
+            // dyGraph.updateOptions({'file': graphData});
             frequencyValue = 0;
-
             time += 5;
         }
 
         frequencyValue++;
     }
-    displayDyGraph(graphData);
+    // displayDyGraph(graphData);
+    dyGraph.updateOptions({'file': graphData});
 }
 
-function displayDyGraph(graphData){
+function displayDyGraph(data){
     if (resetButton == null){
         resetButton = document.createElement('input');
         resetButton.type = 'button';
@@ -141,13 +137,14 @@ function displayDyGraph(graphData){
         document.getElementById("reset-chat-button").style.visibility = "visible";
     }
 
-    dyGraph = new Dygraph(
-
-        // containing div
+    dyGraph = new Dygraph(        
         document.getElementById("frequencyChart"),
-        graphData, //data
-        {   //options
+        data, 
+        {   
             clickCallback: graphClickCallback,
+            highlightCircleSize: 5,
+            valueFormatter: formatMouseoverDisplay,
+            legend: "follow",
             labels: ['Time', 'Count'],
             axes: {
                 x: {
@@ -249,10 +246,25 @@ function graphClickCallback(evt, x, points){
 }
 
 function formatTimeDisplay(seconds, granularity, opts, graph){
-    var date = new Date(null);
-    date.setSeconds(seconds); 
-    var result = date.toISOString().substr(11, 8);
-    return result;
+    return convertSeconds(seconds);
+}
+
+function formatMouseoverDisplay(value, opts, seriesName, dygraph, row, col){
+    if (seriesName == "Count"){
+        return value;
+    }    
+    return convertSeconds(value);
+}
+
+function convertSeconds(sec_num) {
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
 }
 
 function unzoomGraph() {
@@ -262,7 +274,7 @@ function unzoomGraph() {
     });
   }
 
-function pullChatLog( messageArray, obj){
+function pullChatLog( messageArray, obj, currentTime, currentFreqValue, currentFreqArray){
     var clientId = 'oe92qc609eaxxhoh4h5s06pvz7gd9l';
     var twitchChatUrlPrefix = 'https://api.twitch.tv/kraken/videos/';
     var twitchChatUrlSuffix = '/comments';
@@ -281,6 +293,12 @@ function pullChatLog( messageArray, obj){
     xhr.setRequestHeader("Client-ID", clientId);
     xhr.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
 
+    if (currentTime == null && currentFreqValue == null){
+        currentTime = 0;
+        currentFreqValue = 0;
+        currentFreqArray = [];
+    }
+
     xhr.onload = function(){
         console.log("Pulled chat log from Twitch");
         var result = xhr.responseText;
@@ -289,10 +307,19 @@ function pullChatLog( messageArray, obj){
 
         for (var i in obj.comments){
             messageArray.push(new ChatMessage(obj.comments[i].content_offset_seconds, obj.comments[i].message.body));
+
+            if (obj.comments[i].content_offset_seconds > currentTime + 5){
+                currentFreqArray.push([currentTime,currentFreqValue]);
+                currentFreqValue = 0;
+                currentTime += 5;
+            }
+    
+            currentFreqValue++;
         }
+        dyGraph.updateOptions({'file': currentFreqArray});  //dynamically updates graph with frequency data
 
         if (obj._next != null){
-            pullChatLog(messageArray, obj);
+            pullChatLog(messageArray, obj, currentTime, currentFreqValue, currentFreqArray);
         }
         else{
             chatlogs[videoId] = messageArray;
@@ -300,230 +327,8 @@ function pullChatLog( messageArray, obj){
             chrome.storage.local.set({'chatlogs': chatlogs}, function() {
                 console.log('Value is set to ' + chatlogs);
             });
-
-            parseSavedChatLog(messageArray);
         }
     };
     xhr.send();
-
-}
-
-function myMain (evt) {
-    //var canvasContainer = document.createElement("canvasContainer");
-    // canvasContainer.style.width = '100%';
-    //canvasContainer.innerHTML = '<canvas id="frequencyChart"></canvas>'
-    var canvas = document.createElement('canvas');
-    canvas.id = 'frequencyChart';
-    var scrollArea = document.getElementsByClassName('tw-pd-t-2 tw-pd-x-3')[0]; //.appendChild(canvas);
-    scrollArea.insertBefore(canvas, scrollArea.childNodes[2]);
-    //document.getElementById('root').appendChild(canvasContainer);
-
-
-    //TODO: implement first pass filter that filters for spikes in comment frequency, then do in depth analysis on those moments
-
-    // import Chart from './Chart.bundle';
-
-    var clientId = 'oe92qc609eaxxhoh4h5s06pvz7gd9l';
-
-    var videoId = (document.URL).split("/")[4];
-
-    var twitchChatUrlPrefix = 'https://api.twitch.tv/kraken/videos/';
-    var twitchChatUrlSuffix = '/comments';
-    var twitchChatUrlInitial = '?content_offset_seconds=0';
-    var twitchChatUrlCursor = '?cursor=';
-
-    class ChatMessage {
-        constructor(time, text) {
-            this.time = time;
-            this.text = text;
-        }
-    }
-
-    class Highlight {
-        constructor(time, text, popularity){
-            this.time = time;
-            this.text = text;
-            this.popularity = popularity;
-        }
-    }
-
-    class Frequency{
-        constructor(time, value){
-            this.time = time;
-            this.value = value;
-        }
-    }
-
-    var frequencyData = [];
-    var timeData = []
-
-    var xhr = new XMLHttpRequest();
-    var chatMessageArray = [];
-    var obj = null;
-    var wordsMap = {};   // hash map that will find most popular text string every 10 seconds
-    var highlightList = [];
-    var frequencyList = [];
-    var time = 5;
-    var frequencyValue = 0;
-
-    do {
-        if (obj == null){
-            url = twitchChatUrlPrefix + videoId + twitchChatUrlSuffix + twitchChatUrlInitial;
-        }
-        else{
-            url = twitchChatUrlPrefix + videoId + twitchChatUrlSuffix + twitchChatUrlCursor + obj._next;
-        }
-
-        xhr.open("GET", url, false);
-        xhr.setRequestHeader("Client-ID", clientId);
-        xhr.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
-
-        xhr.send();
-        console.log("Pulled data from Twitch");
-        var result = xhr.responseText;
-
-        var obj = JSON.parse(result);
-
-        for (var i in obj.comments){
-            // if (time > 300){
-            //     break;
-            // }
-            if (obj.comments[i].content_offset_seconds > time){
-                // frequencyList.push(new Frequency(time,frequencyValue));
-                frequencyData.push(frequencyValue);
-
-                frequencyValue = 0;
-
-                timeData.push(secondsToHms(time));
-
-                time += 5;
-
-                //find most popular words in wordmap and add to highlights map
-                //empty out wordsMap
-                //TODO background processing
-                // var max = 0;
-                // var maxIndex = null;
-                // for (var word in wordsMap){
-                //     if (wordsMap[word] > max){
-                //         max = wordsMap[word];
-                //         maxIndex = word;
-                //     }
-                // }
-                
-                // highlightList.push(new Highlight(time-10,maxIndex,max));
-
-                // wordsMap = {};
-
-            }
-
-            frequencyValue++;
-
-            //TODO background processing
-            // var wordsArray = (obj.comments[i].message.body).split(" ");
-            // wordsArray.forEach(function (key) {
-            //     if (wordsMap.hasOwnProperty(key)) {
-            //         wordsMap[key]++;
-            //     } else {
-            //         wordsMap[key] = 1;
-            //     }
-            // });
-
-            chatMessageArray.push(new ChatMessage(obj.comments[i].content_offset_seconds, obj.comments[i].message.body));
-
-        }
-
-    } while (obj._next != null);
-
-    chatlogs[videoId] = chatMessageArray;
-    //save storage
-    chrome.storage.local.set({'chatlogs': chatlogs}, function() {
-        console.log('Value is set to ' + chatlogs);
-    });
-
-    var ctx = document.getElementById('frequencyChart').getContext('2d');
-
-    var frequencyChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: timeData,
-            datasets: [{
-                label: 'Frequency',
-                data: frequencyData
-            }]
-        },
-        options: {
-            animation: {
-                duration: 0, // general animation time
-            },
-            hover: {
-                animationDuration: 0, // duration of animations when hovering an item
-            },
-            responsiveAnimationDuration: 0, // animation duration after a resize
-            elements: {
-                line: {
-                    tension: 0, // disables bezier curves
-                }
-            },
-            pan: {
-                // Boolean to enable panning
-                enabled: true,
-        
-                // Panning directions. Remove the appropriate direction to disable 
-                // Eg. 'y' would only allow panning in the y direction
-                mode: 'xy',
-                // rangeMin: {
-                //     // Format of min pan range depends on scale type
-                //     x: null,
-                //     y: null
-                // },
-                // rangeMax: {
-                //     // Format of max pan range depends on scale type
-                //     x: null,
-                //     y: null
-                // },
-                // Function called once panning is completed
-                // Useful for dynamic data loading
-                //onPan: function() { console.log('I was panned!!!'); }
-            },
-            
-            // Container for zoom options
-            zoom: {
-                // Boolean to enable zooming
-                enabled: true,
-        
-                // Enable drag-to-zoom behavior
-                drag: true,
-        
-                // Zooming directions. Remove the appropriate direction to disable 
-                // Eg. 'y' would only allow zooming in the y direction
-                mode: 'x',
-                // rangeMin: {
-                //     // Format of min zoom range depends on scale type
-                //     x: null,
-                //     y: null
-                // },
-                // rangeMax: {
-                //     // Format of max zoom range depends on scale type
-                //     x: null,
-                //     y: null
-                // },
-                // Function called once zooming is completed
-                // Useful for dynamic data loading
-                //onZoom: function() { console.log('I was zoomed!!!'); }
-            }
-        }
-    });
-
-
-    // TODO background processing
-    // highlightList.sort(function(a,b){
-    //     return a.popularity - b.popularity;
-    // })
-
-    // for (var i in highlightList){
-    //     console.log(highlightList[i].text + ", " + highlightList[i].time + ", " + highlightList[i].popularity );
-    // }
-
-    // alert("video id is " + videoId + ", response is " + obj);
 
 }
